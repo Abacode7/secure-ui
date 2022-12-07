285342dfdc20eee2c9855391f5a2c3e6f7e4749c
ab <olamipoabraham@gmail.com>
2022-12-07 16:37:14 +0100

    Update client app to fetch details from resource server using tokens

--- src/main/java/com/example/secureui/AppController.java
+++ src/main/java/com/example/secureui/AppController.java
@@ -1,17 +1,35 @@
 package com.example.secureui;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Flux;
 
 @Controller
 public class AppController {
+    @Autowired
+    private WebClient webClient;
+
     @GetMapping("/")
     public String loadHome(){
         return "home";
     }
 
     @GetMapping("/details")
-    public String loadDetails(){
+    public String loadDetails(Model m){
+        Flux<TollStation> response = this.webClient.get()
+        .uri("http://localhost:8082/api/tollStations")
+        .retrieve()
+        .bodyToFlux(TollStation.class);
+
+        List<TollStation> tollStations = response.collectList().block();
+        m.addAttribute("tollStations", tollStations);
+
         return "details";
     }
 }
